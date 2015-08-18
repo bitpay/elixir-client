45e2ac9e664a1bccdb33bd60d19ffb0224b61029
Paul Daigle <paul@bitpay.com>
2015-08-18 15:52:39 -0400

    add generic get method, change error handling
    No longer raises errors, but returns {:error, message} for most
    functions.
    Generic get method is for signed messages, such as token retrieval.

--- lib/bitpay/webclient.ex
+++ lib/bitpay/webclient.ex
@@ -22,7 +22,7 @@ defmodule BitPay.WebClient do
 
   Response: a key/value pair such as `%{pos: 92hgkeit92392gjgj}`
   """
-  def pair_pos_client code, client \\ %BitPay.WebClient{} do
+  def pair_pos_client code, client do
     pair_pos_client code, (code =~ ~r/^\p{Xan}{7}$/), client
   end
 
@@ -37,14 +37,12 @@ defmodule BitPay.WebClient do
   Response: A map corresponding to the data section of the JSON response from the server.
   """
   def create_invoice params, webclient do
-    validate_invoice_args params.price, params.currency
-    uri = webclient.uri <> "/invoices"
-    body = JSX.encode(params) |> elem(1)
-    x_identity = KeyUtils.compressed_public_key webclient.pem
-    x_signature = KeyUtils.sign(uri <> body, webclient.pem)
-    response = post_to_server(uri, body, ["content-type": "application/json", "accept": "application/json", "X-accept-version": "2.0.0", "x-identity": x_identity, "x-signature": x_signature ])
-    process_invoice response.body, response.status, response.success
+    validate_price(params.price, params.currency) |>
+    validate_currency(params.currency) |>
+    check_tokens(params, webclient) |>
+    create_invoice_with_valid_params(params, webclient)
   end
+
 
   @doc """
   Retrieves an invoice from the BitPay server.
@@ -61,7 +59,23 @@ defmodule BitPay.WebClient do
     uri = webclient.uri <> "/invoices/" <> id
     response = HTTPotion.get(uri) |>
                parse_response
-    process_invoice response.body, response.status, response.success
+    process_data response.body, response.status, response.success
+  end
+
+  @doc """
+  Generic Get method to the WebClient endpoint.
+  Input: 
+    * the endpoint
+    * params for the request
+    * client making the request
+
+    Response:
+      an HTTPotion response object
+  """
+  def get path, webclient do
+    uri = "#{webclient.uri}/#{path}"
+    headers = signed_headers uri, webclient.pem
+    HTTPotion.get uri, headers, []
   end
 
   defp pair_pos_client code, true, client do
@@ -69,8 +83,7 @@ defmodule BitPay.WebClient do
     process_pairing response.body, response.status, response.success
   end
 
-  defp pair_pos_client(_code, false, _client),
-    do: raise(BitPay.ArgumentError, message: "pairing code is not legal")
+  defp pair_pos_client(_code, false, _client), do: {:error, "pairing code is not legal"}
 
   defp process_pairing body, _status, true do
     data = body.data |> List.first
@@ -81,13 +94,47 @@ defmodule BitPay.WebClient do
 
   defp process_pairing(body, status, false), do: process_response(body, status, false)
 
-  defp process_invoice(body, _status, true), do: body.data
+  defp check_tokens({:error, message}, _, _), do: {:error, message}
+  defp check_tokens({:ok, _}, params, webclient) do
+    if params[:token] do
+      {:ok, "token passed in"}
+    else
+      response = get("tokens", webclient) |> parse_response
+      process_data(response.body, response.status, response.success) |>
+      extract_token
+    end
+  end
 
-  defp process_invoice(body, status, false), do: process_response(body, status, false)
+  defp extract_token({:error, message}), do: {:error, message}
+  defp extract_token({:ok, data}) do
+    Enum.map(data, fn(item) -> item[:pos] || item[:merchant] end) |>
+    Enum.filter(fn(i)->i end) |>
+    select_token    
+  end
+
+  defp select_token([]), do: {:error, "no merchant or pos tokens on server"}
+  defp select_token(tokens), do: {:ok, %{token: Enum.at(tokens, 0)}}
+
+  defp create_invoice_with_valid_params({:ok, %{token: token}}, params, webclient) do
+    params = Map.merge(params, %{token: token})
+    create_invoice_with_valid_params({:ok, "token added to params"}, params, webclient)
+  end
+
+  defp create_invoice_with_valid_params({:ok, _}, params, webclient) do
+    uri = webclient.uri <> "/invoices"
+    body = JSX.encode(params) |> elem(1)
+    response = post_to_server(uri, body, signed_headers(uri <> body, webclient.pem))
+    process_data response.body, response.status, response.success
+  end
+
+  defp create_invoice_with_valid_params({:error, error}, _params, _webclient ), do: {:error, error}
+  defp process_data(body, _status, true), do: {:ok, body.data}
+
+  defp process_data(body, status, false), do: process_response(body, status, false)
 
   defp process_response body, status, false do
     message = body.error
-    raise BitPay.BitPayError, message: "#{status}: #{message}"
+    {:error, "#{status}: #{message}"}
   end
 
   defp pair_with_server code, webclient do
@@ -95,12 +142,19 @@ defmodule BitPay.WebClient do
     sin = KeyUtils.get_sin_from_pem(webclient.pem)
     body = JSX.encode(["pairingCode": code, "id": sin]) |>
            elem(1)
-    post_to_server(uri, body, ["content-type": "application/json", "accept": "application/json", "X-accept-version": "2.0.0"])
+    post_to_server(uri, body, [])
   end
 
   defp post_to_server uri, body, headers do
+    headers = Enum.concat(["content-type": "application/json", "accept": "application/json", "X-accept-version": "2.0.0"], headers)
     HTTPotion.post(uri, body, headers) |>
     parse_response
+  end
+
+  defp signed_headers message, pem do
+    x_identity = KeyUtils.compressed_public_key pem
+    x_signature = KeyUtils.sign(message, pem)
+    [ "x-signature": x_signature, "x-identity": x_identity ]
   end
 
   defp parse_response response do
@@ -110,14 +164,18 @@ defmodule BitPay.WebClient do
     %{status: status, body: body, success: success}
   end
 
-  defp validate_invoice_args(price, currency) do
-    price_correct = is_integer(price) || is_float(price) || (is_binary(price) && price =~ ~r/^\d+(\.\d\d)?$/) || (is_list(price) && List.to_string(price) =~ ~r/^\d+(\.\d\d)?$/)
-    currency_correct = currency =~ ~r/^\p{Lu}{3}$/
-    invoice_args_errors(price_correct, currency_correct)
-  end
+  defp validate_price(price, _currency) when is_integer(price), do: {:ok, "price is valid"}
+  defp validate_price(price, _currency) when is_float(price), do: {:ok, "price is valid"}
+  defp validate_price(price, currency) when is_list(price), do: validate_price(List.to_string(price), currency)
+  defp validate_price(price, "BTC") when is_binary(price), do: invoice_args_errors(price =~ ~r/^\d+(\.\d{1,8})?$/, true)
+  defp validate_price(price, _currency) when is_binary(price), do: invoice_args_errors(price =~ ~r/^\d+(\.\d{1,2})?$/, true)
 
-  defp invoice_args_errors(false, _),  do: raise(BitPay.ArgumentError, message: "Illegal Argument: Price must be formatted as a float")
-  defp invoice_args_errors(_, false), do: raise(BitPay.ArgumentError, message: "Illegal Argument: Currency is invalid.")
-  defp invoice_args_errors(_, _), do: true
+  defp validate_currency({:ok, message}, currency) when is_list(currency), do: validate_currency({:ok, message}, List.to_string(currency))
+  defp validate_currency({:ok, _}, currency) when is_binary(currency), do: invoice_args_errors(true, currency =~ ~r/^\p{Lu}{3}$/)
 
+  defp validate_currency({:error, message}, _), do: {:error, message}
+
+  defp invoice_args_errors(true, true), do: {:ok, "no invoice argument error"}
+  defp invoice_args_errors(false, _),  do: {:error, "Illegal Argument: Price must be formatted as a float"}
+  defp invoice_args_errors(_, false), do: {:error, "Illegal Argument: Currency is invalid."}
 end
--- test/webclient_test.exs
+++ test/webclient_test.exs
@@ -3,31 +3,54 @@ defmodule WebClientTest do
   alias BitPay.WebClient, as: WebClient
   import Mock
 
+  test 'get method' do
+    with_mock HTTPotion, [get: fn("https://bitpay.com/blocks", _body, _headers) ->
+        %HTTPotion.Response{status_code: 228, body: "{\"data\":[{\"pos\":\"EBtD6Dae9VXvK8ky7zYkCMfwZCzcsGsDiEfqmZB3Et9K\"},{\"pos/invoice\":\"ED2H47jWZbQKnPTwRLeZcfQ7eN9NyiRFVnRexmoWLScu\"}]}"} end] do
+        assert WebClient.get("blocks", %WebClient{}).status_code == 228 end end
+
   test 'pairing short circuits with invalid code' do
     illegal_pairing_codes() |> Enum.each fn(item) -> 
-      assert_raise BitPay.ArgumentError, "pairing code is not legal", fn() -> WebClient.pair_pos_client item end 
+      assert WebClient.pair_pos_client(item, %WebClient{}) == {:error, "pairing code is not legal"} 
     end
   end
 
   test 'pairing handles errors gracefully' do
-      with_mock HTTPotion, [post: fn("https://bitpay.com/tokens", _body, _headers) ->
-                          %HTTPotion.Response{status_code: 403,
-                                              body: "{\n  \"error\": \"this is a 403 error\"\n}"} end] do
-      assert_raise BitPay.BitPayError, "403: this is a 403 error", fn() ->  WebClient.pair_pos_client "aBD3fhg" end
-    end
+    with_mock HTTPotion, [post: fn("https://bitpay.com/tokens", _body, _headers) ->
+        %HTTPotion.Response{status_code: 403,
+          body: "{\n  \"error\": \"this is a 403 error\"\n}"} end] do
+            assert {:error, "403: this is a 403 error"} ==  WebClient.pair_pos_client "aBD3fhg", %WebClient{}
+          end
   end
 
   test "create invoice fails gracefully with improper price" do
-      params = %{price: "100,00", currency: "USD", token: "anything"}
-      assert_raise BitPay.ArgumentError, "Illegal Argument: Price must be formatted as a float", fn() -> WebClient.create_invoice(params, %BitPay.WebClient{}) end
+    params = %{price: "100,00", currency: "USD", token: "anything"}
+    assert {:error, "Illegal Argument: Price must be formatted as a float"} == WebClient.create_invoice(params, %BitPay.WebClient{})
   end
-  test "create invoice fails gracefully with improper currency" do
-      params = %{price: "100.00", currency: "USDa", token: "anything"}
-      assert_raise BitPay.ArgumentError, "Illegal Argument: Currency is invalid.", fn() -> WebClient.create_invoice(params, %BitPay.WebClient{}) end
+
+  test "create invoice retrieves tokens from server" do
+    params = %{price: 100, currency: "USD"}
+    with_mock HTTPotion, [get: fn("https://bitpay.com/tokens", _body, _headers) -> %HTTPotion.Response{status_code: 228, body: "{\"data\":[{\"pos\":\"EBtD6Dae9VXvK8ky7zYkCMfwZCzcsGsDiEfqmZB3Et9K\"},{\"pos/invoice\":\"ED2H47jWZbQKnPTwRLeZcfQ7eN9NyiRFVnRexmoWLScu\"}]}"} end, post: fn("https://bitpay.com/invoices", "{\"currency\":\"USD\",\"price\":100,\"token\":\"EBtD6Dae9VXvK8ky7zYkCMfwZCzcsGsDiEfqmZB3Et9K\"}", _headers) -> %HTTPotion.Response{status_code: 200, body: "{\"facade\":\"pos/invoice\",\"data\":{\"url\":\"https://test.bitpay.com/invoice?id=A41p1sHqruaA38YaKC83L7\",\"status\":\"new\",\"btcPrice\":\"0.388736\",\"btcDue\":\"0.388736\",\"price\":100,\"currency\":\"USD\",\"exRates\":{\"USD\":257.24390946963365},\"invoiceTime\":1439829619654,\"expirationTime\":1439830519654,\"currentTime\":1439829619916,\"guid\":\"ebf5fe06-b738-4d51-bb9c-f5dcd6613124\",\"id\":\"A41p1sHqruaA38YaKC83L7\",\"btcPaid\":\"0.000000\",\"rate\":257.24,\"exceptionStatus\":false,\"paymentUrls\":{\"BIP21\":\"bitcoin:mtzuBK37L5CxsvPVfP1E711gk6h7VJQ3rJ?amount=0.388736\",\"BIP72\":\"bitcoin:mtzuBK37L5CxsvPVfP1E711gk6h7VJQ3rJ?amount=0.388736&r=https://test.bitpay.com/i/A41p1sHqruaA38YaKC83L7\",\"BIP72b\":\"bitcoin:?r=https://test.bitpay.com/i/A41p1sHqruaA38YaKC83L7\",\"BIP73\":\"https://test.bitpay.com/i/A41p1sHqruaA38YaKC83L7\"},\"token\":\"8CWU2YMJSozc2SKoYJygRkuqmyiuCAQSQrShMpzGT4f77BPABLt9EovsxkmVkUWvPE\"}}" } end] do 
+    {:ok, response} = WebClient.create_invoice(params, %WebClient{}) 
+    assert 1439829619916 ==  response.currentTime 
+            end
   end
-  #        raise BitPay::ArgumentError, "Illegal Argument: Price must be formatted as a float" unless ( price.is_a?(Numeric) || /^[[:digit:]]+(\.[[:digit:]]{2})?$/.match(price) )
-  #        raise BitPay::ArgumentError, "Illegal Argument: Currency is invalid." unless /^[[:upper:]]{3}$/.match(currency)                                                        
-  defp illegal_pairing_codes do
-    ["abcdefgh", "habcdefg", "abcd-fg"]
+
+  test "create invoice handles BTC" do
+    params = %{price: "0.004", currency: "BTC", token: "anything"}
+    with_mock HTTPotion, [post: fn("https://bitpay.com/invoices", _body, _headers) ->
+        %HTTPotion.Response{status_code: 200,
+          body: "{\"facade\":\"pos/invoice\",\"data\":{\"url\":\"https://test.bitpay.com/invoice?id=A41p1sHqruaA38YaKC83L7\",\"status\":\"new\",\"btcPrice\":\"0.388736\",\"btcDue\":\"0.388736\",\"price\":100,\"currency\":\"USD\",\"exRates\":{\"USD\":257.24390946963365},\"invoiceTime\":1439829619654,\"expirationTime\":1439830519654,\"currentTime\":1439829619916,\"guid\":\"ebf5fe06-b738-4d51-bb9c-f5dcd6613124\",\"id\":\"A41p1sHqruaA38YaKC83L7\",\"btcPaid\":\"0.000000\",\"rate\":257.24,\"exceptionStatus\":false,\"paymentUrls\":{\"BIP21\":\"bitcoin:mtzuBK37L5CxsvPVfP1E711gk6h7VJQ3rJ?amount=0.388736\",\"BIP72\":\"bitcoin:mtzuBK37L5CxsvPVfP1E711gk6h7VJQ3rJ?amount=0.388736&r=https://test.bitpay.com/i/A41p1sHqruaA38YaKC83L7\",\"BIP72b\":\"bitcoin:?r=https://test.bitpay.com/i/A41p1sHqruaA38YaKC83L7\",\"BIP73\":\"https://test.bitpay.com/i/A41p1sHqruaA38YaKC83L7\"},\"token\":\"8CWU2YMJSozc2SKoYJygRkuqmyiuCAQSQrShMpzGT4f77BPABLt9EovsxkmVkUWvPE\"}}" } end] do
+          {:ok, response} = WebClient.create_invoice(params, %BitPay.WebClient{})
+          assert 1439829619916 ==  response.currentTime 
+        end
+    end
+
+    test "create invoice fails gracefully with improper currency" do
+      params = %{price: 100.00, currency: "USDa", token: "anything"}
+      assert {:error, "Illegal Argument: Currency is invalid."} == WebClient.create_invoice(params, %BitPay.WebClient{})
+    end
+
+    defp illegal_pairing_codes do
+      ["abcdefgh", "habcdefg", "abcd-fg"]
+    end
   end
-end
